cc10248b2c9a6bbdeb28bcd414d4f15d8fd57be2
Sara Fuller <149854566+sarafuller2018@users.noreply.github.com>
2023-12-01 15:06:06 -0600

    adding more code to JavaScript file

--- script.js
+++ script.js
@@ -7,20 +7,71 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+}
 
-  function userInput() {
-    var userChoiceLowercase = window.confirm("Would you like to include a lowercase letter(s)?");
-    var userChoiceUppercase = window.confirm("Would you like to include a uppercase letter(s)?");
-    var userChoiceNumbers = window.confirm("Would you like to include a number(s)?");
-    var MinLengthQuestion = window.prompt("What is the minimum length for your new password? Type a whole number greater than or equal to 8.");
-    var MaxLengthQuestion = window.prompt("What is the maximum length for your new password? Type a whole number less than or equal to 128.");
+var generatePassword = function () {
+  var userChoiceLowercase = window.confirm("Would you like to include a lowercase letter(s)?");
+  var userChoiceUppercase = window.confirm("Would you like to include a uppercase letter(s)?");
+  var userChoiceNumbers = window.confirm("Would you like to include a number(s)?");
+  var userChoiceChar = window.confirm("Would you like to include a special character(s)?");
+  var MinLengthQuestion = window.prompt("What is the minimum length for your new password? Type a whole number greater than or equal to 8.");
+  var MaxLengthQuestion = window.prompt("What is the maximum length for your new password? Type a whole number less than or equal to 128.");
+
+  if (userChoiceLowercase === false && userChoiceUppercase === false && userChoiceNumbers === false && userChoiceChar === false) {
+    alert("You must choose at least one character type!")
   }
 
-  userInput();
-
-  var generatePassword = function() {
-    //start creating function to generate password
+  if (MinLengthQuestion < 8) {
+    alert("Please enter a number greater than or equal to 8.")
   }
+
+  if (MaxLengthQuestion > 128) {
+    alert("Please enter a number less than or equal to 128.")
+  }
+
+  var characterPool = {
+    Lowercase: userChoiceLowercase,
+    Uppercase: userChoiceUppercase,
+    Numbers: userChoiceNumbers,
+    SpecCharacters: userChoiceChar,
+    Min: MinLengthQuestion,
+    Max: MaxLengthQuestion
+  }
+  console.log(characterPool);
+ 
+// this is where i am lost
+// some type of array??? 
+// ??? var randomPasswordCharacters = ["abcdefghijklmnopqrstuvwxyz", "ABCDEFGHIJKLMNOPQRSTUVWXYZ", "1234567890", "!@#$%^&*()_+[]{}/><;:"];
+
+  for (var i = 0; i < characterPool.Max; i++) {
+    if (userChoiceLowercase) {
+      const lowercaseAlphabet = 'abcdefghijklmnopqrstuvwxyz';
+      const randomLowercaseLetter = Math.floor(Math.random() * lowercaseAlphabet.length);
+      var storeLower = lowercaseAlphabet[randomLowercaseLetter];
+      console.log(storeLower);
+    }
+  
+    if (userChoiceUppercase) {
+      const uppercaseAlphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+      const randomUppercaseLetter = Math.floor(Math.random() * uppercaseAlphabet.length);
+      var storeUpper = uppercaseAlphabet[randomUppercaseLetter]
+      console.log(storeUpper);
+    }
+  
+    if (userChoiceNumbers) {
+      const numbers = '1234567890';
+      const randomNumbers = Math.floor(Math.random() * numbers.length);
+      var storeNumbers = numbers[randomNumbers];
+      console.log(storeNumbers);
+    }
+  
+    if (userChoiceChar) {
+      const specialCharacter = '!@#$%^&*()_+[]{}/><;:';
+      const randomSpecialCharacter = Math.floor(Math.random() * specialCharacter.length);
+      var storeChar = specialCharacter[randomSpecialCharacter]
+      console.log(storeChar);
+  }
+}
 }
 
 // Add event listener to generate button
--- scriptTEMP-DELETE.js
+++ scriptTEMP-DELETE.js
@@ -1,83 +1,8 @@
-// var generatePassword = function () {
-//   userInput();
-// console.log(userChoiceLowercase);
-// console.log(userChoiceUppercase);
-// }
-//   var userChoiceLowercase = window.confirm("Would you like to include a lowercase letter(s)?");
+  //Questions: how do I select more than one option from the string?
 
-//   if (userChoiceLowercase) {
-//     const lowercaseAlphabet = 'abcdefghijklmnopqrstuvwxyz';
-//     const randomLowercaseLetter = Math.floor(Math.random() * lowercaseAlphabet.length);
-//     var storeLower = lowercaseAlphabet[randomLowercaseLetter];
-//     console.log(storeLower);
-//   } else {
-//     return // need to find out what to put here
-//   }
+  //parse through answer choices after questions 
+  //store them in an object
+  //then randomize
+  //then display
 
-//   var userChoiceUppercase = window.confirm("Would you like to include a uppercase letter(s)?");
-
-//   if (userChoiceUppercase) {
-//     const uppercaseAlphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-//     const randomUppercaseLetter = Math.floor(Math.random() * uppercaseAlphabet.length);
-//     var storeUpper = uppercaseAlphabet[randomUppercaseLetter]
-//     // console.log(storeUpper);
-//   } else {
-//     return // need to find out what to put here
-//   }
-
-//   var userChoiceNumbers = window.confirm("Would you like to include a number(s)?");
-
-//   if (userChoiceNumbers) {
-//     const numbers = '1234567890';
-//     const randomNumbers = Math.floor(Math.random() * numbers.length);
-//     var storeNumbers = numbers[randomNumbers];
-//     // console.log(storeNumbers);
-//   } else {
-//     return // need to find out what to put here
-//   }
-
-//   var userChoiceChar = window.confirm("Would you like to include a special character(s)?");
-
-//   if (userChoiceChar) {
-//     const specialCharacter = '!@#$%^&*()_+[]{}/><;:';
-//     const randomSpecialCharacter = Math.floor(Math.random() * specialCharacter.length);
-//     var storeChar = specialCharacter[randomSpecialCharacter]
-//     // console.log(storeChar);
-//   } else {
-//     return // need to find out what to put here
-//   }
-
-//   if (!userChoiceLowercase, !userChoiceLowercase, !userChoiceNumbers, !userChoiceChar) {
-//     windows.alert("You must pick at least one of the previous options. Try again!")
-//   } // ????????
-  
-//   //Questions: how do I select more than one option from the string?
-//   //Where do I actually store the values?
-//   //What to do for "else"?
-//   //how do i check for at least one input and make them redo if not?
-
-//   var MinLengthQuestion = window.prompt("What is the minimum length for your new password? Type a whole number greater than or equal to 8.");
-//   if (MinLengthQuestion >= 8) {
-//     // console.log(MinLengthQuestion)
-//   } else {
-//     alert("Please enter a number greater than or equal to 8.")
-//   }
-
-//   var MaxLengthQuestion = window.prompt("What is the maximum length for your new password? Type a whole number less than or equal to 128.");
-
-//   if (MaxLengthQuestion <= 128) {
-//     console.log(MaxLengthQuestion)
-//   } else {
-//     alert("Please enter a number less than or equal to 128.")
-//   }
-
-//   //how do i make the password know to use the store variables for min and max length? 
-//   // parse through answer choices after questions 
-//   //store them in an object
-//   //then randomize
-//   //then display
-
-//   // get then validate then pass to generate function with several steps - build pool of validate characters and randomly select for the length of the password
-// }
-
-
+  // get then validate then pass to generate function with several steps - build pool of validate characters and randomly select for the length of the password
